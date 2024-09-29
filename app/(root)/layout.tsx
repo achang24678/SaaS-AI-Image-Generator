19baa05d6c3c4d37ce6b5425bd8a920c33627f14
Sheng Yu Chang <achang24678@gmail.com>
2024-09-29 15:56:30 -0700

    test branch2

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,21 +1,20 @@
-import MobileNav from '@/components/shared/MobileNav'
-import Sidebar from '@/components/shared/Sidebar'
-import { Toaster } from '@/components/ui/toaster'
-import React from 'react'
+import MobileNav from "@/components/shared/MobileNav";
+import Sidebar from "@/components/shared/Sidebar";
+import { Toaster } from "@/components/ui/toaster";
+import React from "react";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
     return (
         <main className="root">
             <Sidebar />
             <MobileNav />
+            {/* added */}
             <div className="root-container">
-                <div className="wrapper">
-                    {children}
-                </div>
+                <div className="wrapper">{children}</div>
             </div>
             <Toaster />
         </main>
-    )
-}
+    );
+};
 
-export default Layout
+export default Layout;
