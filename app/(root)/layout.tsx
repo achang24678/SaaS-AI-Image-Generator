44dc59789313d19fc6ad97704da0d17598fa0e50
Sheng Yu Chang <achang24678@gmail.com>
2024-09-29 16:00:17 -0700

    test merge branch

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -6,6 +6,7 @@ import React from "react";
 const Layout = ({ children }: { children: React.ReactNode }) => {
     return (
         <main className="root">
+            {/* added */}
             <Sidebar />
             <MobileNav />
             {/* added */}
