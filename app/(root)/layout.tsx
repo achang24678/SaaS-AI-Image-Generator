d4d97455d9080005206de82f6e94d3b048e0a952
Sheng Yu Chang <achang24678@gmail.com>
2024-09-29 17:22:27 -0700

    Revert "test merge branch"
    This reverts commit 26ab2b44d71908a01c7201f432eee2862ca0efef.

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -6,7 +6,6 @@ import React from "react";
 const Layout = ({ children }: { children: React.ReactNode }) => {
     return (
         <main className="root">
-            {/* added */}
             <Sidebar />
             <MobileNav />
             {/* added */}
